d9c91d4d7938b1e8c87d90ceb60dc871d51d8aae
船田 祐太 <32141@ojs.ac.jp>
2021-05-21 15:21:00 +0900

    P49 リスト2.3

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -8,6 +8,28 @@ namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
 
+            if (args.Length >= 1 && args[0] == "-tom") {
+                // フィートからメートルへの対応表を出力
+                for (int feet = 1; feet <= 10; feet++) {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+                }
+            } else {
+                // メートルからフィートへの対応表を出力
+                for (int meter = 1; meter <= 10; meter++) {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+                }
+            }
+        }
+
+        // フィートからメートルを求める
+        static double FeetToMeter(int feet) {
+            return feet * 0.3048;
+        }
+        // メートルからフィートを求める
+        static double MeterToFeet(int meter) {
+            return meter / 0.3048;
         }
     }
 }
