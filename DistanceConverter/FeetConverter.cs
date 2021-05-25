051c1c925eb7ddfd9c2a9927333dd7db17b651b6
船田 祐太 <32141@ojs.ac.jp>
2021-05-25 11:06:19 +0900

    P55リスト2.10 定数を定義（0.3048を定義化）

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -7,14 +7,16 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     //フィートとメートルの単位変換クラス 
     public static class FeetConverter {
+        private const double raito = 0.3048;
+
         //メートルからフィートを求める
         public static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / raito;
         }
 
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * raito;
         }
     }
 }
