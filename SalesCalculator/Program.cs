e750ff325aee0a4f25c91f3d358bf1cd02cafdb7
船田 祐太 <32141@ojs.ac.jp>
2021-05-25 11:58:08 +0900

    P60リスト2.15 売上データ読み込みメソッド作成

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,27 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
+
+
+
         }
+
+        static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+
+                sales.Add(sale);
+            }
+            return sales;
+
+
+        }   
     }
 }
