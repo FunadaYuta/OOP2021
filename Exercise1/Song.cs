9971289abc46ee3080a663d8ea16e80c36acf362
船田 祐太 <32141@ojs.ac.jp>
2021-05-28 14:25:13 +0900

    P79

---
+++ Exercise1/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Song {
+        string Title;//歌のタイトル
+        string ArtistName;//アーティスト名
+        int Length;//演奏時間、単位は秒
+
+    }
+}
