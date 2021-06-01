69b49e2582122dc8c6b066603668b0a6f9cc76f7
船田 祐太 <32141@ojs.ac.jp>
2021-06-01 10:57:17 +0900

    P79問題2.3完成

---
+++ Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("sales.csv");
+
+            var amountPerStore = sales.GetPerCategorySales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
---
+++ Exercise3/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    //売上クラス
+    class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+
+        //商品カテゴリ
+        public string ProductCategory { get; set; }
+
+        //売上高
+        public int Amount { get; set; }
+
+        
+    }
+}
