7d7acc17cb516a8625da1164a0b7f48fa6ab1d3e
祐太 船田 <32141@ojs.ac.jp>
2021-08-26 15:39:33 +0900

    リスト11.7まで

---
+++ Chapter11/Section01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var xdoc = XDocument.Load("novelists.xml");
+            foreach (var xnovelist in xdoc.Root.Elements()) {
+                var xname = xnovelist.Element("name");
+                var works = xnovelist.Element("masterpieces").
+                    Elements("title").Select(x => x.Value);
+                Console.WriteLine("{0} - {1}",
+                    xname.Value, string.Join(",", works));
+
+            }
+        }
+    }
+}
