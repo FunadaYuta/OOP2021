0b4dd1e7ae1990d68258f573089e949a12c23517
祐太 船田 <32141@ojs.ac.jp>
2021-08-27 14:16:42 +0900

    リスト11.10

--- Chapter11/Section01/Program.cs
+++ Chapter11/Section01/Program.cs
@@ -8,18 +8,32 @@ using System.Xml.Linq;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var xdoc = XDocument.Load("novelists.xml");
-            var novelists = xdoc.Root.Elements().Select(x => new {
-                Name = (string)x.Element("name"),
-                Birth = (DateTime)x.Element("birth"),
-                Death = (DateTime)x.Element("death"),
-            });
-
+            var novelists = ReadNovelists();
             foreach (var novelist in novelists) {
-                Console.WriteLine("{0} ({1}-{2})",novelist.Name,novelist.Birth.Year,novelist.Death.Year);
+                Console.WriteLine("{0} ({1} - {2}) - {3}",novelist.Name,novelist.Birth.Year,novelist.Death.Year,
+                    string.Join(",",novelist.Masterpieces));
             }
         }
+
+        static public IEnumerable<Novelist> ReadNovelists() {
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                .Select(x => new Novelist{
+                    Name = (string)x.Element("name"),
+                    KanaName = (string)(x.Element("name").Attribute("kana")),
+                    Birth = (DateTime)x.Element("birth"),
+                    Death = (DateTime)x.Element("death"),
+                    Masterpieces = x.Element("masterpieces")
+                                     .Elements("title")
+                                     .Select(title => title.Value)
+                                     .ToArray()
+
+                });
+            return novelists.ToArray();
+        }
+
+
+
     }
 }
 
-        
