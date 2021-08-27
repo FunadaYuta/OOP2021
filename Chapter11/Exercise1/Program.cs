3f7081d1b51f3bbca143ef8190242f35b6242688
祐太 船田 <32141@ojs.ac.jp>
2021-08-27 15:34:15 +0900

    P299 11_3途中

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+
+
+            var file = "Sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine("----------");
+
+            Exercisce1_2(file);
+            Console.WriteLine("----------");
+
+
+            Exercise1_3(file);
+            Console.WriteLine("----------");
+
+
+
+
+
+
+
+            var xdoc = XDocument.Load("Sample.xml");
+
+            
+
+            Console.WriteLine("-----------------");
+
+            
+
+            Console.WriteLine("-----------------");
+
+
+            var sample3 = xdoc.Root.Elements().Select(z => z.Element("name").Value);
+            Console.WriteLine(sample3);
+
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load(file);
+            var sample1 = xdoc.Root.Elements();
+            foreach (var sample in sample1) {
+                var xname = sample.Element("name");
+                var team = sample.Element("teammembers");
+                Console.WriteLine("{0},{1}", xname.Value, team.Value);
+            }
+        }
+
+        private static void Exercisce1_2(string file) {
+            var xdoc = XDocument.Load(file);
+            var sample2 = xdoc.Root.Elements().OrderBy(x => (string)x.Element("firstplayed"));
+            foreach (var sample in sample2) {
+                var xname = sample.Element("name").Attribute("kanji");
+                Console.WriteLine(xname.Value);
+            }
+        }
+
+
+        private static void Exercise1_3(string file) {
+            var xdoc = XDocument.Load(file);
+            var max = xdoc.Elements().OrderBy(x => x.Element("teammembers")).Max();
+            Console.WriteLine(max.Element("name").Value);
+            
+            
+            
+            
+        }
+    }
+}
