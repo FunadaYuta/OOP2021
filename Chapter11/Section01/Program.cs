0c69b238e5bfc078c712e155259ea3210090e6bf
祐太 船田 <32141@ojs.ac.jp>
2021-08-27 13:41:20 +0900

    リスト11.9　

--- Chapter11/Section01/Program.cs
+++ Chapter11/Section01/Program.cs
@@ -9,14 +9,17 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var xdoc = XDocument.Load("novelists.xml");
-            foreach (var xnovelist in xdoc.Root.Elements()) {
-                var xname = xnovelist.Element("name");
-                var works = xnovelist.Element("masterpieces").
-                    Elements("title").Select(x => x.Value);
-                Console.WriteLine("{0} - {1}",
-                    xname.Value, string.Join(",", works));
+            var novelists = xdoc.Root.Elements().Select(x => new {
+                Name = (string)x.Element("name"),
+                Birth = (DateTime)x.Element("birth"),
+                Death = (DateTime)x.Element("death"),
+            });
 
+            foreach (var novelist in novelists) {
+                Console.WriteLine("{0} ({1}-{2})",novelist.Name,novelist.Birth.Year,novelist.Death.Year);
             }
         }
     }
 }
+
+        
