a4a125046b1b7c1c2c8c24b6557f10cdc55298b7
祐太 船田 <32141@ojs.ac.jp>
2021-08-30 14:17:06 +0900

    リスト11.12

---
+++ Chapter11/Section02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            string elmstring =
+              @"<novelist>
+                  <name kana=""きくち かん"">菊池 寛</name>
+                  <birth>1888-12-26</birth>
+                  <death>1948-03-06</death>
+                  <masterpieces>
+                    <title>恩讐の彼方に</title>
+                    <title>真珠夫人</title>
+                  </masterpieces>
+                </novelist>";
+
+            XElement element = XElement.Parse(elmstring);
+
+            var xdoc = XDocument.Load("novelists.xml");
+            xdoc.Root.Add(element);
+
+            xdoc.Save("novelists.xml");//XMLファイルへの保存
+        }
+    }
+}
