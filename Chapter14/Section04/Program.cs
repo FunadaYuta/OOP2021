137ac1b989db9f482b62036dfd1979c8428d69b4
祐太 船田 <32141@ojs.ac.jp>
2021-09-02 16:00:56 +0900

    リスト14.19

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            new Program();
+        }
+
+        //コンストラクタ
+        public Program() {
+            //DownloadString();
+            //DownloadFileAsync()
+            //OpenReadSample();
+            var results = GetWeatherReportFromYahoo(4210);
+            foreach(var s in results) {
+                Console.WriteLine(s);
+                Console.ReadLine();
+            }
+        }
+
+        //リスト14.15
+        public void DownloadString() {
+            var wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            var html = wc.DownloadString("https://www.yahoo.co.jp/");
+            Console.WriteLine(html);
+        }
+
+        //リスト14.17
+        private void DownloadFileAsync() {
+            var wc = new WebClient();
+            var url = new Uri(@"C:\Users\yf32141\Downloads\8.zip");
+            var filename = @"C:\temp\example.zip";
+            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+            wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+            wc.DownloadFileAsync(url, filename);
+            Console.ReadLine();//アプリケーションが終了しないようにする
+        }
+
+        static void wc_DownloadProgressChanged(object sender,
+                            DownloadProgressChangedEventArgs e) {
+            Console.WriteLine("{0}% {1}/{2}", e.ProgressPercentage,
+                              e.BytesReceived, e.TotalBytesToReceive);
+        }
+
+        static void wc_DownloadFileCompleted(object sender,
+                            System.ComponentModel.AsyncCompletedEventArgs e) {
+            Console.WriteLine("ダウンロード完了");
+        }
+
+        //リスト14.18
+        public void OpenReadSample() {
+            var wc = new WebClient();
+            using (var stream = wc.OpenRead("https://yahoo.co.jp/"))
+            using (var sr = new StreamReader(stream, Encoding.UTF8)) {
+                string html = sr.ReadToEnd();
+                Console.WriteLine(html);
+            }
+
+        }
+
+        //リスト14.19
+        private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                    @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml", cityCode);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes) {
+                    string s = Regex.Replace(node.Value, "【|】", "");
+                    yield return s;
+                }
+            }
+        }
+
+    }
+}
