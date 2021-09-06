5804b1d27eac985afc66e8c82f03d128346dcfd2
祐太 船田 <32141@ojs.ac.jp>
2021-09-06 15:02:23 +0900

    タイトル表示完成

---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btRead_Click(object sender, EventArgs e) {
+            setRssTitle(tbUrl.Text);
+
+
+        }
+
+        private void setRssTitle(string url) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var urll = new Uri(url);
+                var stream = wc.OpenRead(urll);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes) {
+                    lbTitles.Items.Add(Regex.Replace(node.Value, "【|】", ""));
+                }
+            }
+        }
+
+    }
+}
